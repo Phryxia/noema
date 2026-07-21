b3d714a1670c3ea236f296bce7127fe672e81c46
Phryxia <xahhaepica@gmail.com>
2026-07-22 02:37:57 +0900

    refactor: extract `RecentLine` to reuse at `DocumentWriter`
    - note that `WordWriter` is not compatible as it uses inline flex

---
+++ packages/client/src/recent/RecentLines/RecentLines.tsx
@@ -0,0 +1,19 @@
+import type { ReactElement, ReactNode } from 'react'
+import classnames from 'classnames/bind'
+import styles from './RecentLines.module.css'
+
+const cx = classnames.bind(styles)
+
+interface RecentLinesProps {
+  title: string
+  children: ReactNode
+}
+
+export function RecentLines({ title, children }: RecentLinesProps): ReactElement {
+  return (
+    <article className={cx('root')}>
+      <h2>{title}</h2>
+      <ul className={cx('list')}>{children}</ul>
+    </article>
+  )
+}
--- packages/client/src/sentence/RecentSentences/RecentSentences.tsx
+++ packages/client/src/sentence/RecentSentences/RecentSentences.tsx
@@ -1,12 +1,9 @@
 import type { ReactElement } from 'react'
 import { Link } from '@tanstack/react-router'
 import { useQuery } from '@tanstack/react-query'
+import { RecentLines } from '../../recent/RecentLines/RecentLines'
 import { RECENT_SENTENCES_QUERY_KEY } from '../consts'
 import { getRecentSentences } from '../sentence.service'
-import classnames from 'classnames/bind'
-import styles from './RecentSentences.module.css'
-
-const cx = classnames.bind(styles)
 
 export function RecentSentences(): ReactElement {
   const { data: recentSentences } = useQuery({
@@ -15,20 +12,14 @@ export function RecentSentences(): ReactElement {
   })
 
   return (
-    <article className={cx('root')}>
-      <h2>최근에 추가된 문장</h2>
-      <ul className={cx('list')}>
-        {recentSentences?.map((sentence) => (
-          <li key={sentence.sentenceId}>
-            <Link
-              to="/sentence/$sentenceId"
-              params={{ sentenceId: String(sentence.sentenceId) }}
-            >
-              {sentence.value}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </article>
+    <RecentLines title="최근에 추가된 문장">
+      {recentSentences?.map((sentence) => (
+        <li key={sentence.sentenceId}>
+          <Link to="/sentence/$sentenceId" params={{ sentenceId: String(sentence.sentenceId) }}>
+            {sentence.value}
+          </Link>
+        </li>
+      ))}
+    </RecentLines>
   )
 }

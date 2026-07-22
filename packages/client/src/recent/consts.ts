e4b81b7ba1183b312e1f2e46d8746d0f9dcdd1b1
Phryxia <xahhaepica@gmail.com>
2026-07-23 06:38:47 +0900

    improve: shorten table size into 10 entries
    because it was too long to fit in non scroll page

--- packages/client/src/recent/consts.ts
+++ packages/client/src/recent/consts.ts
@@ -2,6 +2,6 @@ export const RECENT_WORD_PAGES_QUERY_KEY = 'recentWordPages'
 export const RECENT_SENTENCE_PAGES_QUERY_KEY = 'recentSentencePages'
 export const RECENT_DOCUMENT_PAGES_QUERY_KEY = 'recentDocumentPages'
 
-export const RECENT_PAGE_SIZE = 32
+export const RECENT_PAGE_SIZE = 10
 export const VALUE_PREVIEW_LENGTH = 48
 export const SOURCE_PREVIEW_LENGTH = 16

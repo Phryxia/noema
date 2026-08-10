a2f914e3b92ce5263359f25cc10980791e3f4e3c
Phryxia <xahhaepica@gmail.com>
2026-08-10 23:13:37 +0900

    refactor: use shared `RadioGroup` for `RelationPage`
    - removed redundant component relaying
    - migrate css into folder of page's component

--- packages/client/src/relation/RelationPage/RelationPage.tsx
+++ packages/client/src/relation/RelationPage/RelationPage.tsx
@@ -1,9 +1,15 @@
+import classnames from 'classnames/bind'
+import styles from './RelationPage.module.css'
 import type { ReactElement } from 'react'
+import { RadioGroup } from '../../shared/RadioGroup'
 import { RelationForm } from '../RelationForm/RelationForm'
-import { RelationTypeSelector } from '../RelationTypeSelector/RelationTypeSelector'
 import type { RelationEditor } from '../useRelationEditor'
 import { MetaFields } from '../../meta/MetaFields/MetaFields'
 import type { MetaField } from '../../meta/MetaFields/MetaFields'
+import type { QuestionType } from '../../question/types'
+import { QuestionTypeSpecs } from '../../explore/consts'
+
+const cx = classnames.bind(styles)
 
 interface RelationPageProps {
   title: string
@@ -11,6 +17,11 @@ interface RelationPageProps {
   meta?: MetaField[]
   hasRandomPick?: boolean
 }
+
+const QuestionOptions = QuestionTypeSpecs.map((spec) => ({
+  value: spec.type,
+  label: spec.label,
+}))
 
 export function RelationPage({
   title,
@@ -22,10 +33,13 @@ export function RelationPage({
     <article>
       <h2>{title}</h2>
       {meta && <MetaFields fields={meta} />}
-      <RelationTypeSelector
-        type={editor.type}
-        isEditable={editor.isTypeEditable}
+      <RadioGroup<QuestionType>
+        className={cx('radio-group')}
+        name="relationType"
+        value={editor.type}
+        options={QuestionOptions}
         onChange={editor.setType}
+        disabled={!editor.isTypeEditable}
       />
       <hr />
       <RelationForm editor={editor} hasRandomPick={hasRandomPick} />
